9db927e72e26875797a20f569ddb7940abf1c6f9
LDA <dalubimov@vtb.ru>
2025-10-23 17:38:04 +0300

    first init

---
+++ src/main/java/com/aston/study/AstonConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.aston.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonConfigServerApplication.class, args);
+	}
+
+}
